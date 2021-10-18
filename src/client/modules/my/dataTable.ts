69913c312611fab341a5b1e1c6ff6a61d6c0379f
Elizabeth Bauch <ebauch@mymail.mines.edu>
2021-10-18 14:25:58 -0600

    Cleaned up code changes

--- src/client/modules/my/dataTable.ts
+++
@@ -1,8 +0,0 @@
-// // dataTable.ts
-// import { getRawResults } from 'my/resultsFetcher';
-// ...
-//     async connectedCallback() {
-//         this.data = await getRawResults();
-//         OR
-//         this.data = extractPlayerStats(await getRawResults());
-//     }
