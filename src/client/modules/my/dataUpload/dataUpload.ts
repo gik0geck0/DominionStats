ae802d64d9ea6f2134a2be9e16c57d72ecaaef58
Tanner Lorenz <tlorenz@mymail.mines.edu>
2021-11-01 13:13:43 -0600

    Added basic data form

---
+++ src/client/modules/my/dataUpload/dataUpload.ts
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class DataUploader extends LightningElement {
+
+    /**
+     * Click handler for the submit button.
+     */
+    handleClick() {
+
+        console.log("Submit button clicked!");
+
+    }
+
+}
