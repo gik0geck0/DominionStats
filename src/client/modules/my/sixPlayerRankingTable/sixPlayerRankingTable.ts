f1ddb59d662de5663e88eff4983fd793fb99f42d
elibillinger <eebillin@hotmail.com>
2021-11-10 00:19:47 -0700

    Added tables for 2-6 player games

---
+++ src/client/modules/my/sixPlayerRankingTable/sixPlayerRankingTable.ts
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+import { extractPlayerStats, getRawResults } from 'my/resultsFetcher';
+
+
+export default class sixPlayerRankingTable extends LightningElement {
+
+    tableData = [];
+    async connectedCallback() {
+        this.tableData = extractPlayerStats(await getRawResults(),6);
+    }
+}
+
+
+
+
+
