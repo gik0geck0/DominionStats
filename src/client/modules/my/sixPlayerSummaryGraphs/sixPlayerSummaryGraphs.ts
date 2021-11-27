55954e1a285387e926fbd9fceb3dedcca1a207f5
elibillinger <eebillin@hotmail.com>
2021-11-27 01:45:57 -0700

    added bar chart of average points

--- src/client/modules/my/sixPlayerSummaryGraphs/sixPlayerSummaryGraphs.ts
+++ src/client/modules/my/sixPlayerSummaryGraphs/sixPlayerSummaryGraphs.ts
@@ -1,5 +1,5 @@
 import { LightningElement } from 'lwc';
-import {getRawResults, extractaGameSizePlayerStats } from 'my/resultsFetcher';
+import { getRawResults,extractaGameSizePlayerStats } from 'my/resultsFetcher';
 import type { GameResultsData, PlayerStatsAllGames } from 'my/resultsFetcher';
 
 // allows type-completion of the global-variable D3, which is assumed to already have been loaded (from script tag)
@@ -14,10 +14,16 @@ interface DonutData {
     name: string;
     value: number;
 }
+interface AvgPointsData {
+    player_name: string;
+    avg_points: number;
+}
 
 export default class SummaryGraphs extends LightningElement {
+    //gameParticipationTrendData: PlayersPerGame[] = [];
     firstPlaceFreqDonutData: DonutData[] = [];
     gamesPlayedDonuntData: DonutData[] = [];
+    avgPointsWonBarData: AvgPointsData[] = [];
     hasRendered = false;
     scalePoint = d3.scalePoint;
 
@@ -40,7 +46,7 @@ export default class SummaryGraphs extends LightningElement {
                     }
                     return 0;
                 });
-             // Games Played Most Frequent
+            // Games Played Most Frequent
             this.gamesPlayedDonuntData = playerOverviewStats
                 .filter(ps => ps.num_games > 1)
                 .map((ps) => { return {name: ps.player_name, value: ps.num_games}; })
@@ -51,7 +57,17 @@ export default class SummaryGraphs extends LightningElement {
                         return 1;
                     }
                     return 0;
-            });
+                });
+            // Average Points
+            this.avgPointsWonBarData = playerOverviewStats
+                .sort((a,b) => {
+                    if (a.avg_points < b.avg_points) {
+                        return 1;
+                    } else if (a.avg_points > b.avg_points) {
+                        return -1;
+                    }
+                    return 0;
+                });
         } else {
             console.log("Blocked a re-render propagation");
         }
